b7047f259edb0c8a9d95fbcc6d3ee4af52df75fb
agusalvs <agustina.alvarezsacra@gmail.com>
2023-01-26 15:20:57 +0000

    Build a StarWars REST API v2

--- src/models.py
+++ src/models.py
@@ -6,14 +6,105 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    likes = db.relationship('Likes', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+#     addresses = db.relationship('Address', backref='person', lazy=True)
+class People(db.Model):
+    # Here we define columns for the table people
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+    homeworld = db.Column(db.String(250), nullable=False)
+    starship = db.Column(db.String(250), nullable=False)
+    likes = db.relationship('Likes', backref='people', lazy=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "homeworld": self.homeworld,
+            "starship": self.starship,
+        }
+
+
+class Starships(db.Model):
+    # Here we define columns for the table people
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    model = db.Column(db.String(250), nullable=False)
+    starship_class = db.Column(db.String(250), nullable=False)
+    passengers = db.Column(db.String(250), nullable=False)
+    likes = db.relationship('Likes', backref='starships', lazy=True)
+
+
+    def __repr__(self):
+        return '<Starships %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "starship_class": self.starship_class,
+            "passengers": self.passengers,
+        }
+
+
+class Planets(db.Model):
+    # Here we define columns for the table people
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.String(250), nullable=False)
+    gravity = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    likes = db.relationship('Likes', backref='planets', lazy=True)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "gravity": self.gravity,
+            "climate": self.climate,
+        }
+
+class Likes(db.Model):
+    # Here we define columns for the table people
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    starships_id = db.Column(db.Integer, db.ForeignKey('starships.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    def __repr__(self):
+        return '<Likes %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "starships_id": self.starships_id,
+            "planets_id": self.planets_id,
         }
