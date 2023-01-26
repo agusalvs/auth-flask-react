50599ca236f96f73e9841a70bb14147ac13a1d75
agusalvs <agustina.alvarezsacra@gmail.com>
2023-01-26 13:24:18 +0000

    Build a StarWars REST API

--- src/app.py
+++ src/app.py
@@ -49,3 +49,35 @@ def handle_hello():
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+# from flask import Flask, jsonify, request
+
+# app = Flask(__name__)
+
+# todos = [
+#     { "label": "My first task", "done": False },
+#     { "label": "My second task", "done": False }
+# ]
+
+# @app.route('/todos', methods=['GET'])
+# def hello_world():
+#     json_text = jsonify(todos)
+#     return json_text
+
+# @app.route('/todos', methods=['POST'])
+# def add_new_todo():
+#     request_body = request.data
+#     todos.append(request.get_json(force=True))
+#     print("Incoming request with the following body", request_body)
+#     return jsonify(todos)
+
+# @app.route('/todos/<int:position>', methods=['DELETE'])
+# def delete_todo(position):
+#     todos.pop(position)
+#     print("This is the position to delete: ",position)
+#     return jsonify(todos)
+
+# # These two lines should always be at the end of your app.py file.
+# if __name__ == '__main__':
+#   app.run(host='0.0.0.0', port=3245, debug=True)
